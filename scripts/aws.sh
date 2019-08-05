e2df8eec990337fa77de6310f3cd98045408959c
Mark Hegedus <hmark33@gmail.com>
2019-08-05 18:32:34 +0200

    fix(travisfile, aws.sh)

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws configure
+aws configure set aws_acces_key_id $AWS_ACCES_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
 aws s3 cp s3://$BUCKETNAME/.env $TRAVIS_BUILD_DIR/.env
