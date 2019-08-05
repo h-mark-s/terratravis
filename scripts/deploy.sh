848f31a664696a28cf0a8e608e174887df5dc7c1
Mark Hegedus <hmark33@gmail.com>
2019-08-05 20:09:02 +0200

    fix(.travis.yml): fix hostname variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,9 @@
 scp $TRAVIS_BUILD_DIR/.env $USERNAME@$PUBLICIP:~/app/appenv/
 rm $TRAVIS_BUILD_DIR/.env
 
-echo $PASSWORD\n | ssh -T $USERNAME@$PUBLICIP
+ssh -T $USERNAME@$PUBLICIP
+sleep 10
+echo $PASSWORD
 sleep 120
 
 cd /home/ubuntu/app/docker && docker pull hmarks/quiz_app:latest
@@ -12,5 +14,5 @@ docker kill my_quiz_app && docker rm my_quiz_app || echo "No such docker contain
 
 docker run --name my_quiz_app -p 5500:5500 -d hmarks/quiz_app
 docker cp /home/ubuntu/app/appenv/.env my_quiz_app:/usr/src/app/
-docker restart my_quiz_app
+docker restart my_quiz_app:
 
